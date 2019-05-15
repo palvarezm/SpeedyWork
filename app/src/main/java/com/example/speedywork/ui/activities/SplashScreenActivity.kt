4ff46343fd793bfad299b9d819dcb6ba968a6b23
Paul <palvareze7@gmail.com>
2019-05-15 02:55:24 -0500

    Add fade animation to login activity

--- app/src/main/java/com/example/speedywork/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/speedywork/ui/activities/SplashScreenActivity.kt
@@ -1,26 +1,35 @@
-package com.example.speedywork
+package com.example.speedywork.ui.activities
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
-import android.view.Window
+import com.example.speedywork.R
+import com.example.speedywork.ui.utils.Animation
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         showWelcomeText()
+        endSplash()
+    }
+
+    private fun endSplash() {
+        Handler().postDelayed({
+            intent = Intent(this@SplashScreenActivity, LoginActivity::class.java)
+            startActivity(intent)
+            Animation.FadeAnimation(this@SplashScreenActivity)
+        },3500)
     }
 
     private fun showWelcomeText() {
         Handler().postDelayed({
             tvWelcomeFirst.visibility = View.VISIBLE
             tvWelcomeSecond.visibility = View.VISIBLE
-        },2000)
+        },1500)
     }
 }
