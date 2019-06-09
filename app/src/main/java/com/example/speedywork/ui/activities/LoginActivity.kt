004d783684dd6e9bd7c81f8bffa09625c4b7ef81
Paul <palvareze7@gmail.com>
2019-06-08 19:53:01 -0500

    Add login screen layout

--- app/src/main/java/com/example/speedywork/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/speedywork/ui/activities/LoginActivity.kt
@@ -3,11 +3,21 @@ package com.example.speedywork.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.speedywork.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        setActions()
     }
+
+    private fun setActions() {
+        btLogin.setOnClickListener {
+
+        }
+    }
+
+
 }
