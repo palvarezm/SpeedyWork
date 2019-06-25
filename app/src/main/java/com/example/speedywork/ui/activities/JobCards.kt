22ca7f3a9c10fa4cfe4668491a042bbafeb76815
Paul <palvareze7@gmail.com>
2019-06-25 00:32:14 -0500

    Add JobCards activity and library

---
+++ app/src/main/java/com/example/speedywork/ui/activities/JobCards.kt
@@ -0,0 +1,13 @@
+package com.example.speedywork.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.speedywork.R
+
+class JobCards : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_job_cards)
+    }
+}
